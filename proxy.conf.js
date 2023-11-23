69e6eb51ea1e9c782d83b8c45619e0f684464b1a
joao.effting <joao.effting@totvs.com.br>
2023-11-23 16:16:52 -0300

    Deploy em prod

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,10 +1,22 @@
 const PROXY_CONFIG = [
   {
     context: ['/api'],
-    target: 'http://localhost:8080/',
+    target: 'http://18.229.159.250:8080',
     secure: false,
     logLevel: 'debug'
   }
 ];
 
 module.exports = PROXY_CONFIG;
+
+// const PROXY_CONFIG = [
+//   {
+//     context: ['/api'],
+//     target: 'http://localhost:8080/',
+//     secure: false,
+//     logLevel: 'debug'
+//   }
+// ];
+
+// module.exports = PROXY_CONFIG;
+
