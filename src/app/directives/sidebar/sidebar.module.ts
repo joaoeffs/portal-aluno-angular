baf477cbe2e04262cf8fb7e74f2922832861d7ff
João Victor Effting <joaoefftings@gmail.com>
2023-11-29 18:11:15 -0300

    Update sidebar.module.ts

--- src/app/directives/sidebar/sidebar.module.ts
+++ src/app/directives/sidebar/sidebar.module.ts
@@ -1,11 +1,9 @@
-// sidebar.module.ts
 import { NgModule } from '@angular/core';
 import { SideBarComponent } from './sidebar.component';
-// ... Importações adicionais
 
 @NgModule({
   declarations: [SideBarComponent],
   exports: [SideBarComponent],
-  // ... Outras configurações do módulo
 })
+  
 export class SideBarModule { }
